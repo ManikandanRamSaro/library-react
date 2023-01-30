ff65212c49bb25481b4486457127ebef53e9b048
mramal958 <mramal958@cable.comcast.com>
2023-01-30 14:56:33 +0530

    package testing commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-//import Card from "./library/Card";
-import Container from "./library/Container";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        This information is used to shape Storybook's roadmap and prioritize
-        features. You can learn more, including how to opt-out if you'd not like
-        to participate in this anonymous program, by visiting the following URL:
-      </Container>
+      This information is used to shape Storybook's roadmap and prioritize
+      features. You can learn more, including how to opt-out if you'd not like
+      to participate in this anonymous program, by visiting the following URL:
     </div>
   );
 }
